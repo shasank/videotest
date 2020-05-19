1f9cfbcc662625bf8aa80ca22ffdb39113998703
shasank.maturi <shasank.maturi@servicenow.com>
2020-05-19 13:21:12 +0530

    dd

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,31 @@
-/*
- *  Copyright (c) 2018 The WebRTC project authors. All Rights Reserved.
- *
- *  Use of this source code is governed by a BSD-style license
- *  that can be found in the LICENSE file in the root of the source
- *  tree.
- */
-'use strict';
-import {LitElement, html} from 'https://unpkg.com/@polymer/lit-element@0.6.2/lit-element.js?module';
+import React from 'react';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+const Home = props =>
+  <div className="home">
+    <div>
+      <h1 itemProp="headline">ServiceNow video chat</h1>
+      <p>Please enter a room name.</p>
+      <input type="text" name="room" value={ props.roomId } onChange={props.handleChange} pattern="^\w+$" maxLength="10" required autoFocus title="Room name should only contain letters or numbers."/>
+      <Link className="primary-button" to={ '/r/' + props.roomId }>Join</Link>
+      <Link className="primary-button" to={ '/r/' + props.defaultRoomId }>Random</Link>
+      { props.rooms.length !== 0 && <div>Recently used rooms:</div> }
+      { props.rooms.map(room => <Link key={room} className="recent-room" to={ '/r/' + room }>{ room }</Link>) }
+    </div>
+  </div>;
+
+Home.propTypes = {
+  handleChange: PropTypes.func.isRequired,
+  defaultRoomId: PropTypes.string.isRequired,
+  roomId: PropTypes.string.isRequired,
+  rooms: PropTypes.array.isRequired
+};
+
+const mapStateToProps = store => ({rooms: store.rooms});
+
+export default connect(mapStateToProps)(Home);
 
 class ScreenSharing extends LitElement {
   constructor() {
